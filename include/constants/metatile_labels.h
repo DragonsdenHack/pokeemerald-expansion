8f977dd3236835494e3c240618dfb4446700a239
DragonsdenHack <eccrouen5991@gmail.com>
2022-10-02 00:34:16 +0200

    Nuevos tiles + detalles oficinas
    Creados tiles para fondos en ordenadores, mesas, sofás, en gráficos de silph y guardia rocket principalmente.
    Cambiados en todos los mapas en que se presentan.

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -400,6 +400,8 @@
 
 // gTileset_Petalburg
 #define METATILE_Petalburg_Door_Oldale  0x287
+#define METATILE_Petalburg_Door_Littleroot 0x248
+#define METATILE_Petalburg_Door_BirchsLab 0x249
 
 // gTileset_PokemonCenter
 #define METATILE_PokemonCenter_CounterBarrier            0x25D
