ad815d81bc14358a3abcecaadd63eb612b48d040
kakarottowah <42473328+kakarottowah@users.noreply.github.com>
2022-10-03 16:46:26 +0200

    error fixeado
    cambio nombre andra por zeus, cambio menor

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -490,7 +490,7 @@
 #define MUS_RG_TRAINER_TOWER        556 // MUS_RG_T_TOWER
 #define MUS_RG_SLOW_PALLET          557 // MUS_RG_SLOWMASARA
 #define MUS_RG_TEACHY_TV_MENU       558 // MUS_RG_TVNOIZE
-#define END_MUS                     MUS_ENCOUNTER_ANDRA
+#define END_MUS                     MUS_BATTLE_ZEUS
 
 #define PH_TRAP_BLEND               559
 #define PH_TRAP_HELD                560
@@ -544,7 +544,7 @@
 #define PH_NURSE_HELD               608
 #define PH_NURSE_SOLO               609
 /// nuevas canciones
-#define MUS_ENCOUNTER_ANDRA         610
+#define MUS_BATTLE_ZEUS         610
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
