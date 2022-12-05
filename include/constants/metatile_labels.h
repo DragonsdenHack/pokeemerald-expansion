a9fc4578b37d8b0ca1ce53db93c7ac2a5b303baf
DragonsdenHack <eccrouen5991@gmail.com>
2022-12-05 02:20:32 +0100

    Trainers básicos PT5
    Trainers básicos insertados y comprobados hasta misión SSANNE y Ruta 9

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -399,9 +399,9 @@
 #define METATILE_PetalburgGym_SlidingDoor_Frame4  0x21C
 
 // gTileset_Petalburg
-#define METATILE_Petalburg_Door_Oldale  0x287
-#define METATILE_Petalburg_Door_Littleroot 0x248
-#define METATILE_Petalburg_Door_BirchsLab 0x249
+#define METATILE_Petalburg_Door_BirchsLab   0x249
+#define METATILE_Petalburg_Door_Littleroot  0x248
+#define METATILE_Petalburg_Door_Oldale      0x287
 
 // gTileset_PokemonCenter
 #define METATILE_PokemonCenter_CounterBarrier            0x25D
