e1b37e097e2e62b247b01b137a387997376ddbcb
kakarottowah <42473328+kakarottowah@users.noreply.github.com>
2022-12-27 21:39:50 +0100

    editado la plantilla de los little boy
    solo para probar

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -3193,27 +3193,27 @@ static const struct SpriteFrameImage sPicTable_Lass2[] = {
 };
 
 static const struct SpriteFrameImage sPicTable_LittleBoy2[] = {
-    overworld_frame(gObjectEventPic_LittleBoy2, 2, 4, 0),
-    overworld_frame(gObjectEventPic_LittleBoy2, 2, 4, 1),
-    overworld_frame(gObjectEventPic_LittleBoy2, 2, 4, 2),
-    overworld_frame(gObjectEventPic_LittleBoy2, 2, 4, 3),
-    overworld_frame(gObjectEventPic_LittleBoy2, 2, 4, 4),
-    overworld_frame(gObjectEventPic_LittleBoy2, 2, 4, 5),
-    overworld_frame(gObjectEventPic_LittleBoy2, 2, 4, 6),
-    overworld_frame(gObjectEventPic_LittleBoy2, 2, 4, 7),
-    overworld_frame(gObjectEventPic_LittleBoy2, 2, 4, 8),
+    overworld_frame(gObjectEventPic_LittleBoy2, 2, 2, 0),
+    overworld_frame(gObjectEventPic_LittleBoy2, 2, 2, 1),
+    overworld_frame(gObjectEventPic_LittleBoy2, 2, 2, 2),
+    overworld_frame(gObjectEventPic_LittleBoy2, 2, 2, 3),
+    overworld_frame(gObjectEventPic_LittleBoy2, 2, 2, 4),
+    overworld_frame(gObjectEventPic_LittleBoy2, 2, 2, 5),
+    overworld_frame(gObjectEventPic_LittleBoy2, 2, 2, 6),
+    overworld_frame(gObjectEventPic_LittleBoy2, 2, 2, 7),
+    overworld_frame(gObjectEventPic_LittleBoy2, 2, 2, 8),
 };
 
 static const struct SpriteFrameImage sPicTable_LittleGirl2[] = {
-    overworld_frame(gObjectEventPic_LittleGirl2, 2, 4, 0),
-    overworld_frame(gObjectEventPic_LittleGirl2, 2, 4, 1),
-    overworld_frame(gObjectEventPic_LittleGirl2, 2, 4, 2),
-    overworld_frame(gObjectEventPic_LittleGirl2, 2, 4, 3),
-    overworld_frame(gObjectEventPic_LittleGirl2, 2, 4, 4),
-    overworld_frame(gObjectEventPic_LittleGirl2, 2, 4, 5),
-    overworld_frame(gObjectEventPic_LittleGirl2, 2, 4, 6),
-    overworld_frame(gObjectEventPic_LittleGirl2, 2, 4, 7),
-    overworld_frame(gObjectEventPic_LittleGirl2, 2, 4, 8),
+    overworld_frame(gObjectEventPic_LittleGirl2, 2, 2, 0),
+    overworld_frame(gObjectEventPic_LittleGirl2, 2, 2, 1),
+    overworld_frame(gObjectEventPic_LittleGirl2, 2, 2, 2),
+    overworld_frame(gObjectEventPic_LittleGirl2, 2, 2, 3),
+    overworld_frame(gObjectEventPic_LittleGirl2, 2, 2, 4),
+    overworld_frame(gObjectEventPic_LittleGirl2, 2, 2, 5),
+    overworld_frame(gObjectEventPic_LittleGirl2, 2, 2, 6),
+    overworld_frame(gObjectEventPic_LittleGirl2, 2, 2, 7),
+    overworld_frame(gObjectEventPic_LittleGirl2, 2, 2, 8),
 };
 
 static const struct SpriteFrameImage sPicTable_Lorelei[] = {
