4a9075886e4350127add9c673171853039bad403
kakarottowah <42473328+kakarottowah@users.noreply.github.com>
2023-01-06 18:06:08 +0100

    fix metatiles y script que comprueba si tienes el poke capturado

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -297,7 +297,7 @@
 #define METATILE_Lilycove_Wailmer2         0x2A0
 #define METATILE_Lilycove_Wailmer3         0x2A1
 
--// gTileset_MauvilleGym
+// gTileset_MauvilleGym
 #define METATILE_MauvilleGym_FloorTile        0x21A
 #define METATILE_MauvilleGym_GreenBeamH1_Off  0x230
 #define METATILE_MauvilleGym_GreenBeamH1_On   0x220
