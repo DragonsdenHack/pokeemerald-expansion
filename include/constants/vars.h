f7689bd0d14a07dc5eab3b42f464b26e1573defc
kakarottowah <42473328+kakarottowah@users.noreply.github.com>
2023-03-03 17:09:37 +0100

    insertada elite 4 hasta lance
    - falta cambiar los metatiles de lance PokemonLeague_LancesRoom_EventScript_SetEntryClosed

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -262,7 +262,7 @@
 #define VAR_SCOTT_FORTREE_CALL_STEP_COUNTER  0x40F3
 #define VAR_ROXANNE_CALL_STEP_COUNTER        0x40F4
 #define VAR_SCOTT_BF_CALL_STEP_COUNTER       0x40F5
-#define VAR_RIVAL_RAYQUAZA_CALL_STEP_COUNTER 0x40F6
+#define VAR_RIVAL_RAYQUAZA_CALL_STEP_COUNTER 0x40F6 // liga kanto
 #define VAR_UNUSED_0x40F7                    0x40F7 // Unused Var // EVENTOS 
 #define VAR_UNUSED_0x40F8                    0x40F8 // Unused Var // PILAR RECUERDO
 #define VAR_UNUSED_0x40F9                    0x40F9 // Unused Var // GIMNASIOS
