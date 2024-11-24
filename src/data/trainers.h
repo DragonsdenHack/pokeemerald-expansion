70c85067db486e782f509e3b0216dc514d2e8624
DragonsdenHack <eccrouen5991@gmail.com>
2024-11-24 12:42:28 +0100

    TESTEO KANTO: Pueblo Paleta
    Makea

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -450,7 +450,7 @@ const struct Trainer gTrainers[] = {
     [TRAINER_DALIA] =
     {
         .partyFlags = F_TRAINER_PARTY_HELD_ITEM | F_TRAINER_PARTY_CUSTOM_MOVESET,
-        .trainerClass = TRAINER_CLASS_RIVALES,
+        .trainerClass = TRAINER_CLASS_FUJI,
         .encounterMusic_gender = F_TRAINER_FEMALE | TRAINER_ENCOUNTER_MUSIC_AQUA,
         .trainerPic = TRAINER_PIC_DALIA,
         .trainerName = _("DALIA"),
