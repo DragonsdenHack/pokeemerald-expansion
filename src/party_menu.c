fbadc7af82f7f050082cce46c68a429f29ec9d9f
kakarottowah <42473328+kakarottowah@users.noreply.github.com>
2024-12-01 20:05:48 +0100

    MOS sin conocerlo en los ataques y traduccion texto
    UNDERWATER ahora es BAJO EL AGUA
    
    Los ataques CORTE, FUERZA, GOLPE ROCA, Y BUCEO pueden ser usados siempre y cuando se tengan en la mochila sin necesidad de que un POKéMON lo conozca. En caso de que sí, aparecera la animación del POKéMON en concreto.
    
    SURF puede ser usado directamente sin hablar con el agua.
    
    Los ataques VUELO y DESTELLO apareceran en el menú de cada pokémon que pueden aprenderlo sin necesidad de que lo tengan. En caso de que el pokemon este lleno de MOS, se dará prioridad a VUELO.

--- src/party_menu.c
+++ src/party_menu.c
@@ -2099,6 +2099,18 @@ static void Task_HandleCancelParticipationYesNoInput(u8 taskId)
     }
 }
 
+static u8 CanTeachMove2(struct Pokemon *mon, u16 move)
+{
+	if (CanLearnTeachableMove(GetMonData(mon, MON_DATA_SPECIES2), move))
+	{
+		return CANNOT_LEARN_MOVE;
+	}else
+	{
+		return CAN_LEARN_MOVE;
+	}
+	
+}	
+
 static u8 CanTeachMove(struct Pokemon *mon, u16 move)
 {
 
@@ -2659,12 +2671,18 @@ static void SetPartyMonFieldSelectionActions(struct Pokemon *mons, u8 slotId)
         {
             if (GetMonData(&mons[slotId], i + MON_DATA_MOVE1) == sFieldMoves[j])
             {
-                AppendToList(sPartyMenuInternal->actions, &sPartyMenuInternal->numActions, j + MENU_FIELD_MOVES);
-                break;
+                if (sFieldMoves[j] != MOVE_FLY) // If Mon already knows FLY, prevent it from being added to action list
+                    if (sFieldMoves[j] != MOVE_FLASH) // If Mon already knows FLASH, prevent it from being added to action list
+                        AppendToList(sPartyMenuInternal->actions, &sPartyMenuInternal->numActions, j + MENU_FIELD_MOVES);
+                        break;
             }
         }
     }
-
+	
+	if (sPartyMenuInternal->numActions < 5 && CanTeachMove2(&mons[slotId], MOVE_FLY)) // 839 - 592 If Mon can learn HM02 and action list consists of < 4 moves, add FLY to action list
+        AppendToList(sPartyMenuInternal->actions, &sPartyMenuInternal->numActions, 5 + MENU_FIELD_MOVES);
+    if (sPartyMenuInternal->numActions < 5 && CanTeachMove2(&mons[slotId], MOVE_FLASH)) // If Mon can learn HM05 and action list consists of < 4 moves, add FLASH to action list
+        AppendToList(sPartyMenuInternal->actions, &sPartyMenuInternal->numActions, 1 + MENU_FIELD_MOVES);
     if (!InBattlePike())
     {
         if (GetMonData(&mons[1], MON_DATA_SPECIES) != SPECIES_NONE)
