c76f443b01cecc70c318b9bfbf094c4e6340c3ac
kakarottowah <42473328+kakarottowah@users.noreply.github.com>
2024-12-11 00:32:18 +0100

    actualizacion parte algaria + se ve bien cubone

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -5407,12 +5407,12 @@ static const struct SpriteFrameImage sPicTable_Cubone[] = {
     overworld_frame(gObjectEventPic_Cubone, 2, 2, 0),
     overworld_frame(gObjectEventPic_Cubone, 2, 2, 1),
     overworld_frame(gObjectEventPic_Cubone, 2, 2, 2),
-    overworld_frame(gObjectEventPic_Cubone, 2, 2, 0),
-    overworld_frame(gObjectEventPic_Cubone, 2, 2, 0),
-    overworld_frame(gObjectEventPic_Cubone, 2, 2, 1),
-    overworld_frame(gObjectEventPic_Cubone, 2, 2, 1),
-    overworld_frame(gObjectEventPic_Cubone, 2, 2, 2),
-    overworld_frame(gObjectEventPic_Cubone, 2, 2, 2),
+    overworld_frame(gObjectEventPic_Cubone, 2, 2, 3),
+    overworld_frame(gObjectEventPic_Cubone, 2, 2, 4),
+    overworld_frame(gObjectEventPic_Cubone, 2, 2, 5),
+    overworld_frame(gObjectEventPic_Cubone, 2, 2, 6),
+    overworld_frame(gObjectEventPic_Cubone, 2, 2, 7),
+    overworld_frame(gObjectEventPic_Cubone, 2, 2, 8),
 };
 
 static const struct SpriteFrameImage sPicTable_Poliwrath[] = {
